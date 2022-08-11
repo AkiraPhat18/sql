88530d8c324fa5b8d3ca39fd04e948194955bb6c
AkiraPhat18 <55183867+AkiraPhat18@users.noreply.github.com>
2022-08-11 21:48:20 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,5 +2,12 @@ create table GiaoVien (
 	MaGV nvarchar(10),
 	Name nvarchar(100)
 )
-
+-- sửa bảng , thêm cột
 alter table HocSinh add NgaySinh Date
+
+-- xóa toàn bộ dữ liệu của bảng
+truncate table HocSinh
+
+--gỡ bảng khỏi DB
+drop table HocSinh
+go
